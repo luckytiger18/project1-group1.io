28a1e74131ae8f9104b6edacb3695072b6bd47fe
Annie Luc <lannie3186@gmail.com>
2019-08-06 21:12:48 -0700

    working API, took out map, pulling zipcode, created buttons for addresses.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,20 +5,25 @@
 // tracking location
 
 function queryMapApi(address) {
-  
+
     var map = "https://api.mapbox.com/geocoding/v5/mapbox.places/playgrounds " + address + ".json?access_token=pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ"
     $.ajax({
         url: map,
         method: "GET"
     }).then(function (response) {
-        // console.log(response)
+        console.log(response)
         // Iterate over the response feature
-        
+
         for (var i = 0; i < response.features.length; i++) {
-            var geocoords = response.features[i].place_name;
-            $("#places").append(geocoords + "<br>");
-            console.log(geocoords)
-            
+            // var geocoords = response.features[i].place_name;
+            var geocoords = response.features[i].context[i].text;
+            var playgrounds = response.features[i].place_name;
+            var category = response.features[i].properties.category;
+            if (category == "playground, leisure") {
+                $("#places").append("<button>" + playgrounds + "<br>");
+            }
+            // console.log(geocoords)
+
         }
     })
 }
@@ -30,11 +35,18 @@ function queryWeatherApi(address) {
         method: "GET"
     }).then(function (response) {
         var temp = response.main.temp;
-        var tempNew = 1.8 * (temp - 273) + 32;
-        $("#temperature").append("Temperature: " + tempNew)
+        var tempNew = parseInt(1.8 * (temp - 273) + 32);
+        var wind = response.wind.speed;
+        var description = response.weather[0].main;
+        $("#weather").append("Temperature: " + tempNew + "<br>" + "Wind: " + wind + "MPH" + "<br>" + "Description: " + description)
+        // console.log(response);
     })
 }
 
+function reset() {
+
+}
+// execution 
 $("#submitButton").on("click", function () {
     event.preventDefault();
     var address = $("#searchLocation").val();
@@ -42,17 +54,17 @@ $("#submitButton").on("click", function () {
     queryMapApi(address);
     queryWeatherApi(address);
 })
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-96, 37.8], // starting position
-    zoom: 3 // starting zoom
-});
-// Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true
-}));
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ';
+// var map = new mapboxgl.Map({
+//     container: 'map', // container id
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [-96, 37.8], // starting position
+//     zoom: 3 // starting zoom
+// });
+// // Add geolocate control to the map.
+// map.addControl(new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true
+//     },
+//     trackUserLocation: true
+// }));
