02db3ed56337ffefed5b4b1c0ffd6266aaa0e851
Serene Deng <dengxiaopu@gmail.com>
2019-08-10 11:33:55 -0700

    change the button style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,83 @@
-//  TODO: Check if place is "playground"
-
-// map api
+//  TODO: Check if place is "playground" based on catergory
 
 // var safty = https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=EXZo2ybfi9amYpepIOggcUyzblMHeIpfh1QhMc80&location="  
 
+// tracking location
+
 function queryMapApi(address) {
+
     var map = "https://api.mapbox.com/geocoding/v5/mapbox.places/playgrounds " + address + ".json?access_token=pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ"
     $.ajax({
         url: map,
         method: "GET"
     }).then(function (response) {
-        // console.log(response)
+        console.log(response)
         // Iterate over the response feature
+
         for (var i = 0; i < response.features.length; i++) {
-            var geocoords = response.features[i].place_name;
-            $("#places").append("Places: " + geocoords + "<br>");
+            // var geocoords = response.features[i].place_name;
+            var geocoords = response.features[i].context[i].text;
+            var playgrounds = response.features[i].place_name;
+            var category = response.features[i].properties.category;
+            if (category == "playground, leisure") {
+
+                // var button = $("<button>").attr("id", "jhfhgfgh").attr("gfhg","hgfhgfgh")
+                var button = $("<button>").attr({
+                    "data-lat": response.features[i].center[1],
+                    "data-lon": response.features[i].center[0]
+                })
+                button.text(playgrounds)
+                button.addClass("location")
+
+
+                $("#places").append(button);
+            }
+            // console.log(geocoords)
+
         }
     })
 }
+
+$(document).on("click", ".location", function () {
+    var lat = $(this).attr("data-lat");
+    var lon = $(this).attr("data-lon");
+    map.setCenter({ lat: lat, lon: lon }); //  map.setCenter({lat, lon}) (shortcuts: if it is the same name, you do not need to repeat it.)
+
+
+    map.addLayer({
+        "id": $(this).text(),
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [lon, lat]
+                    },
+                    "properties": {
+                        "title": "Park",
+                        "icon": "park"
+                    }
+                }]
+            }
+        },
+        "layout": {
+            "icon-image": "{icon}-15",
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+    });
+
+
+
+})
+
+
 // Weather API
 function queryWeatherApi(address) {
     var weather = "https://api.openweathermap.org/data/2.5/weather?q=" + address + "&appid=6c256c8dc9b56a412a1c42874dad6022";
@@ -25,10 +85,19 @@ function queryWeatherApi(address) {
         url: weather,
         method: "GET"
     }).then(function (response) {
-        $("#temperature").append("Temperature: " + response.main.temp)
+        var temp = response.main.temp;
+        var tempNew = parseInt(1.8 * (temp - 273) + 32);
+        var wind = response.wind.speed;
+        var description = response.weather[0].main;
+        $("#weather").append("Temperature: " + tempNew + "</br>" + "Wind: " + wind + "MPH" + "</br>" + "Description: " + description)
+        // console.log(response);
     })
 }
 
+function reset() {
+
+}
+// execution 
 $("#submitButton").on("click", function () {
     event.preventDefault();
     var address = $("#searchLocation").val();
@@ -36,3 +105,51 @@ $("#submitButton").on("click", function () {
     queryMapApi(address);
     queryWeatherApi(address);
 })
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ';
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9'
+// });
+
+navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+    map.setCenter({ lat: position.coords.latitude, lon: position.coords.longitude })
+});
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-79.4512, 43.6568],
+    zoom: 16
+});
+
+// var geocoder = new MapboxGeocoder({
+//     accessToken: mapboxgl.accessToken,
+//     marker: {
+//         color: 'orange'
+//     },
+//     mapboxgl: mapboxgl
+// });
+
+// map.addControl(geocoder);
+
+
+
+
+
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYWtrdXJuaWNraSIsImEiOiJjanl2dGlhZ2gwZXdpM21yb2pzeTN0MXU1In0.fw1d3cJU5L4lFhDYAzy3fQ';
+// var map = new mapboxgl.Map({
+//     container: 'map', // container id
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [-96, 37.8], // starting position
+//     zoom: 3 // starting zoom
+// });
+// // Add geolocate control to the map.
+// map.addControl(new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true
+//     },
+//     trackUserLocation: true
+// }));
